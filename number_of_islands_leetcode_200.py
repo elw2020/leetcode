9f9af7f422ab3d8f4093d03687acc2f5c3784efb
Eric Williamson <45341168+elw2020@users.noreply.github.com>
2023-02-21 11:31:42 -0500

    Add files via upload
    This is my BFS solution to: number of islands (leetcode 200).

---
+++ number_of_islands_leetcode_200.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def getIslandLength(node):
+            stack = [node]
+            while stack:
+                row,col = stack.pop()
+                if grid[row][col] == '*':
+                    continue
+                grid[row][col]='*'
+                if row-1>=0 and grid[row-1][col]=='1':               # up
+                    stack.append([row-1,col])
+                if row+1<len(grid) and grid[row+1][col]=='1':        # down
+                    stack.append([row+1,col])
+                if col-1>=0 and grid[row][col-1]=='1':               # left
+                    stack.append([row,col-1])
+                if col+1 < len(grid[0]) and grid[row][col+1]=='1':   # right
+                    stack.append([row,col+1])
+        numIslands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    getIslandLength([i,j])
+                    numIslands+=1
+        return numIslands
